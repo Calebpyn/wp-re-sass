ac0764b97fdef36603867c1a150e5f7b29338abd
Caleb Payan <calebpayan75@gmail.com>
2024-08-21 10:56:57 -0700

    Removed unused components

--- dashboard-client/src/components/common/AddPropertyDialog.tsx
+++ dashboard-client/src/components/common/AddPropertyDialog.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
 import {
--- dashboard-client/src/components/dashboard/Home.tsx
+++ dashboard-client/src/components/dashboard/Home.tsx
@@ -3,10 +3,6 @@ import GridImageDisplay from "../common/GridImageDisplay";
 
 function Home() {
 
-  const dummy = (_images: Array<string>) => {
-    console.log("No need for a setImagesParent function from home...")
-  }
-
   return <div className="relative p-5">
 
     <GridImageDisplay height="500"/>
